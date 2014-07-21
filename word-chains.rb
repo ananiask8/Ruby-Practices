5fa9a41fab54105311fd043e19ae534ef8c973d0
Ananias Hilario <j.ananias.hilario@gmail.com>
2014-07-21 14:00:08 -0400

    Changed 'unless' for 'next if'

--- word-chains.rb
+++ word-chains.rb
@@ -18,9 +18,7 @@ class WordChainer
 		@current_words = Set.new [source]
 		@all_seen_words = {source => nil}
 
-		until @current_words.empty? || @all_seen_words.include?(target)
-			explore_current_words
-		end
+		explore_current_words until @current_words.empty? || @all_seen_words.include?(target)
 		p build_path(target)
 	end
 
@@ -30,14 +28,14 @@ class WordChainer
 		new_current_words = Set.new []
 		@current_words.each do |current_word|
 			adjacent_words(current_word).each do |adjacent_word|
-				unless @all_seen_words.include?(adjacent_word)
-					@all_seen_words[adjacent_word] = current_word
-					new_current_words << adjacent_word
-				end
+				next if @all_seen_words.include?(adjacent_word)
+					
+				@all_seen_words[adjacent_word] = current_word
+				new_current_words << adjacent_word
 			end
 		end
 		@current_words = new_current_words
-		new_current_words.each{|word| puts "#{word} came from #{@all_seen_words[word]}"}
+		# new_current_words.each{|word| puts "#{word} came from #{@all_seen_words[word]}"}
 	end
 
 	def build_path(target)
@@ -50,4 +48,4 @@ end
 dict_path = '../projects/dictionary.txt'
 chainer = WordChainer.new(dict_path)
 # p chainer.adjacent_words('gain')
-chainer.run('gain', 'stain')
+chainer.run('gain', 'pain')
