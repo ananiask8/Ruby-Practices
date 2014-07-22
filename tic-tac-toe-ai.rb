982c440a00ef5f5e82241638abe4ef8645a93b69
Ananias Hilario <j.ananias.hilario@gmail.com>
2014-07-22 11:18:25 -0400

    Finished. Fixed bug related to calculating the status of the board each time a move is made, and record the winner only the first time we see one (just in case). Also, added method Board#over? which returns true if we have a winner or if the board is filled. Also, added a stop early for losing nodes, as there was a bug that was getting past the first winner (the reason why I noted the precaution stated at the beginning was necessary); with the stop early, I stop as soon as the game is over (Board#over?) and if Board#won? is not true (tie) it is a non losing node, but if it's true, I check if the winner is the opponent in order to classify the node

--- tic-tac-toe-ai.rb
+++ tic-tac-toe-ai.rb
@@ -1,28 +1,46 @@
 
 module TicTacToe
-	MARKS = [false, true]
+	MARKS = [:o, :x]
 end
 class Board
 	include TicTacToe
-	attr_reader :winner, :n
+	attr_reader :n, :spots
+	attr_accessor :winner
 	def initialize(n = 3)
 		@n = n
 		@spots = Array.new(n){Array.new(n)}
 		@winner = nil
 	end
 
-	def won?
-		diag = Array.new(2){Array.new(@n)}
-		@n.times{|i| diag[0][i] = @spots[i][i]}
-		@n.times{|i| diag[1][i] = @spots[i][@n - i]}
-		@spots.each{|i| @winner = i[0] if i.uniq.length == 1 && i[0] != nil}
-		@spots.transpose.each{|i| @winner = i[0] if i.uniq.length == 1 && i[0] != nil}
-		diag.each{|i| @winner = i[0] if i.uniq.length == 1 && i[0] != nil}
-		return @winner? true : false
+	def status_update
+		# First winner remains the winner. 
+		# Added this condition just in case I calculate to the point where 
+		# the board is filled with the SuperComputer and there might be 
+		# two winning players and get an error in predictions because of that.
+		if winner.nil? 
+			diag = Array.new(2){Array.new(@n)}
+			@n.times{|i| diag[0][i] = @spots[i][i]}
+			@n.times{|i| diag[1][i] = @spots[i][@n - i - 1]}
+			@spots.each{|i| @winner = i[0] if i.uniq.length == 1 && !i[0].nil?}
+			@spots.transpose.each{|i| @winner = i[0] if i.uniq.length == 1 && !i[0].nil?}
+			diag.each{|i| @winner = i[0] if i.uniq.length == 1 && !i[0].nil?}
+		end
 	end
 
-	# def winner
-	# end
+	def over?
+		filled? || won?
+	end
+
+	def won?
+		@winner ? true : false
+	end
+
+	def clone
+		clone_board = Board.new(@n)
+		@spots.each_with_index{|col, i| col.each_with_index{|element, j| clone_board.place_mark(@n*i + j, element)}}
+		clone_board.winner = @winner
+		clone_board
+	end
 
 	def empty?(pos)
 		i = pos / @n
@@ -34,11 +52,17 @@ class Board
 		i = pos / @n
 		j = pos % @n
 		@spots[i][j] = mark
+		status_update
 	end
 
 	def filled?
 		(@n * @n).times{|i| return false if empty?(i)}
 		return true
+	end
+
+	def print
+		table = @spots.map{|col| col.map{|elem| elem||=' '; "#{elem}|"}.join}.join("\n")
+		puts "#{table}\n\n"
 	end
 end
 
@@ -54,20 +78,22 @@ class Game
 	def play
 		i = 0
 		until game_over?
-			move = @players[i].make_move(@board)
+			@board.print
+			move = @players[i].make_move(@board.clone)
 			@board.place_mark(move[0], move[1])
 			i = (i == 0) ? 1 : 0
 		end
+		@board.print
 		results
 	end
 
 	def game_over?
-		@board.won? || @board.filled?
+		@board.over?
 	end
 
 	def results
 		return puts "#{@players[0].name} won!!" if @board.winner == @players[0].mark
-		return puts "#{@players[1].name} won!!" unless @board.winner == @players[1].mark
+		return puts "#{@players[1].name} won!!" if @board.winner == @players[1].mark
 		puts "Its a tie.."
 	end
 end
@@ -112,9 +138,25 @@ class ComputerPlayer < Player
 	end
 end
 
+class SuperComputerPlayer < ComputerPlayer
+	def make_move(board)
+		node = TicTacToeNode.new(board, @mark)
+		possible_moves = node.children
+		
+		node = possible_moves.find{|child| child.winning_node?(self)}
+		return [node.prev_move_pos, @mark] if node
+		
+		node = possible_moves.find{|child| !child.losing_node?(self)}
+		return [node.prev_move_pos, @mark] if node
+		
+		raise "Error!"
+	end
+end
+
 class TicTacToeNode
-	attr_reader :next_mover_mark, :prev_move_pos
-	def initialize(board, next_mover_mark, prev_move_pos)
+	include TicTacToe
+	attr_reader :next_mover_mark, :prev_move_pos, :board
+	def initialize(board, next_mover_mark, prev_move_pos = nil)
 		@board = board
 		@next_mover_mark = next_mover_mark
 		@prev_move_pos = prev_move_pos
@@ -122,46 +164,50 @@ class TicTacToeNode
 
 	def children
 		npos = @board.n * @board.n
-		@potential_states = (0...npos).select{|pos| @board.empty?(pos)}.
-					map{|available_pos| TicTacToeNode.new(@board.dup.
-					place_mark(available_pos, next_mover_mark), 
-					!next_mover_mark, available_pos)}
+		new_next = (@next_mover_mark == TicTacToe::MARKS[0]) ? TicTacToe::MARKS[1] : TicTacToe::MARKS[0]
+		potential_states = []
+		(0...npos).each do |available_pos| 
+			next unless @board.empty?(available_pos)
+			new_board = @board.clone
+			new_board.place_mark(available_pos, @next_mover_mark)
+			potential_states << TicTacToeNode.new(new_board, new_next, available_pos)
+		end
+		potential_states
 	end
 
 	def losing_node?(player)
-		@board.filled? && @board.win? != player.mark ||
-		players_turn?(player) && !any_winning_children?(player) ||
-		!players_turn?(player) && any_losing_children?(player)
+		if @board.over?
+			# Have to include 'tied' as a non-losing node.
+			# So, return false if nobody won.
+			return @board.won? && @board.winner != player.mark
+		end
+		
+		if players_turn?(player)
+			children.all?{|child| child.losing_node?(player)}
+		else 
+			children.any?{|child| child.losing_node?(player)}
+		end
 	end
 
 	def winning_node?(player)
-		@board.filled? && @board.win? == player.mark ||
-		players_turn?(player) && any_winning_children?(player) ||
-		!players_turn?(player) && !any_losing_children?(player)
+		if @board.over?
+			return @board.winner == player.mark
+		end
+		
+		if players_turn?(player) 
+			children.any?{|child| child.winning_node?(player)}
+		else
+			children.all?{|child| child.winning_node?(player)}
+		end
 	end
 
 	def players_turn?(player)
-		next_mover_mark == player.mark
-	end
-
-	def any_winning_children?(player)
-		result = false
-		@potential_states.each{|child| result ||= child.winning_node?(player)}
-		result
-	end
-
-	def any_losing_children?(player)
-		result = false
-		@potential_states.each{|child| result ||= child.losing_node?(player)}
-		result
+		@next_mover_mark == player.mark
 	end
 end
 
 human_player = HumanPlayer.new("Ananias")
-cpu1_player = ComputerPlayer.new
-cpu2_player = ComputerPlayer.new
-tic_tac_toe = Game.new(cpu1_player, cpu2_player)
+cpu1_player = SuperComputerPlayer.new
+cpu2_player = SuperComputerPlayer.new
+tic_tac_toe = Game.new(cpu1_player, human_player)
 tic_tac_toe.play
-# b = Board.new
-# tttn = TicTacToeNode.new(b, true, nil)
-# tttn.children
