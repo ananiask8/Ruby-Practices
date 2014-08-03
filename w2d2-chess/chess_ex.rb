f29d48a59039d192d73a6d6c382b7f7436bcca95
Ananias Hilario <j.ananias.hilario@gmail.com>
2014-08-02 22:09:46 -0400

    Added special move for pawn in first move

--- w2d2-chess/chess_ex.rb
+++ w2d2-chess/chess_ex.rb
@@ -119,12 +119,20 @@ end
 
 class Pawn < Piece
 
+  MOVING_DIR = {:white => [[-1, 0], [-1, -1], [-1, 1]], :black => [[1, 0], [1, -1], [1, 1]]}
+
+  def initialize(*args)
+    super
+    @initial_position = @pos
+  end
+
+
   def representation
     "♟"
   end
 
   def move_diffs
-    MOVING_DIR[:pawn][@color]
+    MOVING_DIR[@color]
   end
 
   def moves
@@ -132,18 +140,19 @@ class Pawn < Piece
     moves_to_kill = []
     pre_processing = super
 
-    free_moves << @pos.zip(MOVING_DIR[:pawn][@color][0]).map{|dif| dif.inject(:+)}
-    moves_to_kill << @pos.zip(MOVING_DIR[:pawn][@color][1]).map{|dif| dif.inject(:+)}
-    moves_to_kill << @pos.zip(MOVING_DIR[:pawn][@color][2]).map{|dif| dif.inject(:+)}
+    free_moves << @pos.zip(MOVING_DIR[@color][0]).map{|dif| dif.inject(:+)}
+    moves_to_kill << @pos.zip(MOVING_DIR[@color][1]).map{|dif| dif.inject(:+)}
+    moves_to_kill << @pos.zip(MOVING_DIR[@color][2]).map{|dif| dif.inject(:+)}
 
     results = pre_processing.select{|pos| moves_to_kill.include?(pos) && !@board.empty?(pos)}
     results += pre_processing.select{|pos| free_moves.include?(pos) && @board.empty?(pos)}
+    p results << free_moves[0].zip(MOVING_DIR[@color][0]).map{|dif| dif.inject(:+)} if @pos == @initial_position
   end
 
   def constraint_met?(pos)
     #This new position is farther from what I can get with one move?
     #If so, then the constraint is met
-    @board.out_of_bounds?(pos) || !MOVING_DIR[:pawn][@color].any?{|step| @pos.zip(step).map{|a| a.inject(:+)} == pos}
+    @board.out_of_bounds?(pos) || !MOVING_DIR[@color].any?{|step| @pos.zip(step).map{|a| a.inject(:+)} == pos}
   end
 
 end
@@ -266,7 +275,7 @@ class Board
   end
 
   def draw(&prc)
-    system("clear")
+    # system("clear")
     state = ""
     rows = ""
     state << @grid.map.with_index do |row, j|
