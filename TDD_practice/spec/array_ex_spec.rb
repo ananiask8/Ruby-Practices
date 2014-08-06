0a61239f40acc2a311b069a48c67296efe2cda3b
Ananias Hilario <j.ananias.hilario@gmail.com>
2014-08-06 10:26:23 -0400

    Started project. Working with TDD

---
+++ TDD_practice/spec/array_ex_spec.rb
@@ -0,0 +1,27 @@
+require 'rspec'
+require 'array_ex.rb'
+
+describe Array do
+
+  describe "#my_uniq" do
+    it "removes duplicate elements"
+  end
+
+  describe "#two_sum" do
+    it "outputs pairs that sum to zero"
+
+    it "outputs pairs sorted 'dictionary wise'"
+  end
+
+  describe "#my_transpose" do
+    it "outputs the rows as columns, and viceversa"
+  end
+
+  describe "#stock_picker" do
+    it "outputs most profitable day to buy and to sell"
+  end
+end
+
+describe Hanoi do
+
+end
