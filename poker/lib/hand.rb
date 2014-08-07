9ef34d41dcb3c243a341e2f808f2c1b13713499c
Ananias Hilario <j.ananias.hilario@gmail.com>
2014-08-07 03:19:23 -0400

    A lot of Hand functionality implemented from TDD already

--- poker/lib/hand.rb
+++ poker/lib/hand.rb
@@ -12,12 +12,28 @@ class Hand
             :three_of_a_kind, :two_pair, :one_pair, :high_card
           ]
 
+  class << self
+    def winner(hand_a, hand_b)
+      if RANKINGS.find_index(ranking(hand_a)) == RANKINGS.find_index(ranking(hand_b))
+        return VALUES[hand_a.high_card].last < VALUES[hand_b.high_card].last ? hand_a : hand_b
+        #used last because of A (to take 14)
+      end
+
+      RANKINGS.find_index(ranking(hand_a)) < RANKINGS.find_index(ranking(hand_b)) ? hand_a : hand_b
+    end
+
+    def ranking(hand)
+      RANKINGS.find{|ranking| hand.send((ranking.to_s + "?").to_sym)}
+    end
+  end
+
   def initialize(new_hand)
     @hand = new_hand
   end
 
   def high_card
-    @hand.max{|card_a, card_b| VALUES[card_a] <=> VALUES[card_b]}.value_symbol
+    return "A" if @hand.map(&:value_symbol).include? "A"
+    @hand.max{|card_a, card_b| VALUES[card_a.value_symbol] <=> VALUES[card_b.value_symbol]}.value_symbol
   end
 
   def occurrences(n)
@@ -42,4 +58,29 @@ class Hand
   def four_of_a_kind?
     occurrences(4).size > 0
   end
+
+  def full_house?
+    one_pair? && three_of_a_kind?
+  end
+
+  def straight?
+    copy_hand = @hand.select{|card| card.value_symbol != "A"}
+    sorted_copy = copy_hand.sort{|card_a, card_b| VALUES[card_a.value_symbol] <=> VALUES[card_b.value_symbol]}
+
+    straight_without_a = sorted_copy.select.with_index do |card, i|
+      VALUES[card.value_symbol] == VALUES[sorted_copy.first.value_symbol].map{|value| value + i}
+    end.size == sorted_copy.size
+
+    straight_without_a && (sorted_copy.size == 5 || sorted_copy.first.value_symbol == "2" || sorted_copy.last.value_symbol == "K")
+  end
+
+  def flush?
+    x_type = @hand.sample.type
+    @hand.all?{|card| card.type == x_type}
+  end
+
+  def straight_flush?
+    straight? && flush?
+  end
+
 end
--- poker/spec/hand_spec.rb
+++ poker/spec/hand_spec.rb
@@ -2,30 +2,75 @@ require 'rspec'
 require 'hand.rb'
 
 describe Hand do
+
   describe '#straight_flush?' do
-    # it "accurately detects a straight flush" do
-    #   double("")
-    # end
-  end
-
-  describe '#four_of_a_kind?' do
-
-  end
-
-  describe '#full_house?' do
-
+    it "returns true if all cards have consecutive values and all are of the same color" do
+      hand = Hand.new([
+                          Card.new(1, :diamond), Card.new(5, :diamond), Card.new(3, :diamond),
+                          Card.new(4, :diamond), Card.new(2, :diamond)
+                        ])
+      expect(hand.straight_flush?).to be true
+    end
   end
 
   describe '#flush?' do
-
+    it "returns true if all the cards are of the same type" do
+      hand = Hand.new([
+                          Card.new(1, :diamond), Card.new(5, :diamond), Card.new(3, :diamond),
+                          Card.new(4, :diamond), Card.new(2, :diamond)
+                        ])
+      expect(hand.flush?).to be true
+    end
   end
 
   describe '#straight?' do
+      it "returns true when all the cards have consecutive A->LOW values" do
+        hand = Hand.new([
+                          Card.new(1, :diamond), Card.new(5, :diamond), Card.new(3, :diamond),
+                          Card.new(4, :diamond), Card.new(2, :diamond)
+                        ])
+        expect(hand.straight?).to be true
+      end
 
+      it "returns true when all the cards have consecutive HIGH->A values" do
+        hand = Hand.new([
+                          Card.new(1, :diamond), Card.new(10, :diamond), Card.new(13, :diamond),
+                          Card.new(11, :diamond), Card.new(12, :diamond)
+                        ])
+        expect(hand.straight?).to be true
+      end
+    end
+
+  describe '#four_of_a_kind?' do
+    it "returns true if there is a foursome of cards with same value" do
+      hand = Hand.new([
+            Card.new(1, :diamond), Card.new(1, :spade), Card.new(1, :heart),
+            Card.new(1, :club), Card.new(5, :diamond)
+          ])
+      expect(hand.four_of_a_kind?).to be true
+    end
+  end
+
+  describe '#full_house?' do
+    it "returns true if there is a pair and three of a kind" do
+      hand = Hand.new([
+            Card.new(1, :diamond), Card.new(1, :spade), Card.new(1, :heart),
+            Card.new(4, :diamond), Card.new(4, :spade)
+          ])
+      expect(hand.full_house?).to be true
+    end
+
+    it "doesn't return true if only three of a kind" do
+      hand = Hand.new([
+            Card.new(1, :diamond), Card.new(1, :spade), Card.new(1, :heart),
+            Card.new(4, :diamond), Card.new(5, :diamond)
+          ])
+      expect(hand.full_house?).to be false
+    end
   end
 
   describe '#three_of_a_kind?' do
-    it "returns true if there is a pair of cards with same value" do
+    it "returns true if there is a threesome of cards with same value" do
       hand = Hand.new([
             Card.new(1, :diamond), Card.new(1, :spade), Card.new(1, :heart),
             Card.new(4, :diamond), Card.new(5, :diamond)
@@ -35,7 +80,7 @@ describe Hand do
   end
 
   describe '#two_pair?' do
-    it "returns true if there is a pair of cards with same value" do
+    it "returns true if there are two pairs of cards with same value" do
       hand = Hand.new([
             Card.new(1, :diamond), Card.new(1, :spade), Card.new(3, :diamond),
             Card.new(4, :diamond), Card.new(4, :spade)
@@ -64,7 +109,31 @@ describe Hand do
     end
   end
 
-  describe '#best_hand' do
+  describe '::winner' do
+    it "returns hand that wins" do
+      hand_a = Hand.new([
+                          Card.new(1, :diamond), Card.new(5, :diamond), Card.new(3, :diamond),
+                          Card.new(4, :diamond), Card.new(2, :diamond)
+                        ])
 
+      hand_b = Hand.new([
+            Card.new(1, :diamond), Card.new(1, :spade), Card.new(3, :diamond),
+            Card.new(4, :diamond), Card.new(5, :diamond)
+          ])
+      expect(Hand.winner(hand_a, hand_b)).to be hand_a
+    end
+
+    it "correctly returns a win for same rankings, based on higher card" do
+      hand_a = Hand.new([
+                          Card.new(1, :diamond), Card.new(5, :diamond), Card.new(3, :diamond),
+                          Card.new(4, :diamond), Card.new(2, :diamond)
+                        ])
+
+      hand_b = Hand.new([
+                          Card.new(6, :diamond), Card.new(5, :diamond), Card.new(3, :diamond),
+                          Card.new(4, :diamond), Card.new(2, :diamond)
+                        ])
+      expect(Hand.winner(hand_a, hand_b)).to be hand_b
+    end
   end
 end
